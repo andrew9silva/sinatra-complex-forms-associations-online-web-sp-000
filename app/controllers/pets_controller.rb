9fa9aa6d93f8e173426513f16a1ac4225a582161
Andrew Silva <andrewsilva9@gmail.com>
2019-11-24 00:36:41 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,6 +33,7 @@ class PetsController < ApplicationController
   patch '/pets/:id/' do
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
+    binding.pry
     if !params[:owner]["name"].empty?
       @pet.owner = Owner.create(name: params[:owner]["name"])
     end
